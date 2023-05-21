c3a70e559bcbd010aade594f1e11f1e6e6f4c744
Daniel Rodrigues Alves <drodriguesalves2@gmail.com>
2023-05-20 22:17:38 -0300

    Angular Api - Front

---
+++ src/app/principal/principal.component.ts
@@ -0,0 +1,112 @@
+import {Component} from '@angular/core';
+import {Cliente} from "../modelo/Cliente";
+import {ClienteService} from "../servico/cliente.service";
+
+@Component({
+  selector: 'app-principal',
+  templateUrl: './principal.component.html',
+  styleUrls: ['./principal.component.css']
+})
+export class PrincipalComponent {
+  //Objeto do tipo cliente
+  cliente = new Cliente();
+
+  // Variavel para visibilidade dos botões
+  btnCadastro:boolean = true;
+
+  //Variavel para visibilidade da tabela
+  tabela:boolean = true;
+
+  // JSON de clientes
+  clientes:Cliente[] = [];
+
+  // Construtor
+  constructor(private servico: ClienteService) {
+  }
+
+  // Método de seleção de clientes
+  selecionar():void {
+    this.servico.selecionar()
+      .subscribe(retorno => this.clientes = retorno);
+  }
+
+  //Método de cadastro
+  cadastrar():void {
+    this.servico.cadastrar(this.cliente)
+      .subscribe(retorno => {
+        //Cadastrar o cliente no vetor
+        this.clientes.push(retorno);
+        // Limpar formulário
+        this.cliente = new Cliente();
+        //Mensagem
+        alert("Cliente cadastrado com sucesso!");
+      });
+  }
+
+  //Método para selecionar um cliente especifico
+  selecionarCliente(posicao: number): void {
+    // Selecionar cliente no vetor
+    this.cliente = this.clientes[posicao];
+    // Visibilidade dos botões
+    this.btnCadastro = false;
+    // Visibilidade da tabela
+    this.tabela = false;
+  }
+
+  //Método de cadastro
+  editar():void {
+    this.servico.editar(this.cliente)
+      .subscribe(retorno => {
+        //Obter posição do vetor onde está o cliente
+        let posicao = this.clientes.findIndex(obj => {
+          return obj.codigo == retorno.codigo;
+        })
+        // Alterar os dados do cliente
+        this.clientes[posicao] = retorno
+        // Limpar formulário
+        this.cliente = new Cliente();
+        // Visibilidade dos botões
+        this.btnCadastro = true;
+        // Visibilidade da tabela
+        this.tabela = true;
+        //Mensagem
+        alert("Cliente alterado com sucesso!");
+      });
+  }
+
+  //Método de remoção
+  remover():void {
+    this.servico.remover(this.cliente.codigo)
+      .subscribe(retorno => {
+        //Obter posição do vetor onde está o cliente
+        let posicao = this.clientes.findIndex(obj => {
+          return obj.codigo == this.cliente.codigo;
+        })
+        // Remover cliente do vetor
+        this.clientes.splice(posicao, 1);
+        // Limpar formulário
+        this.cliente = new Cliente();
+        // Visibilidade dos botões
+        this.btnCadastro = true;
+        // Visibilidade da tabela
+        this.tabela = true;
+        //Mensagem
+        alert("Cliente removido com sucesso!");
+      });
+  }
+
+  // Método para cancelar ação
+  cancelar():void{
+    // Limpar formulário
+    this.cliente = new Cliente();
+    // Visibilidade dos botões
+    this.btnCadastro = true;
+    // Visibilidade da tabela
+    this.tabela = true;
+  }
+
+  //Método de inicialização
+  ngOnInit() {
+    this.selecionar();
+  }
+}
